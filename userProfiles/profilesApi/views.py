1a722dea0a3558c132d052fb85a5ca8bcad8840a
Amin Abdisamad <theaminux@gmail.com>
2019-03-29 00:13:42 +0300

    added serializer and post method in view

--- userProfiles/profilesApi/views.py
+++ userProfiles/profilesApi/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+
 
 # Create your views here.
 
 
 class Welcome(APIView):
+    # Serializer class
+    serializer_class = serializers.TestSerializer
+
     def get(self, request, format=None):
         text = [
             "Welcome to Django rest framework",
             "This framework allows you to write rest apis very simply",
         ]
         return Response({"message": text})
+
+    def post(self, request):
+        serializer = serializers.TestSerializer(data=request.data)
+        if serializer.is_valid:
+            name = serializer.data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 class ListAll(APIView):
