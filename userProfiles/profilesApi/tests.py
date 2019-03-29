34c6d84f4ddd828d45d2deef9ab44037d834c487
Amin Abdisamad <theaminux@gmail.com>
2019-03-29 22:42:31 +0300

    added testCase to the api

--- userProfiles/profilesApi/tests.py
+++ userProfiles/profilesApi/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class APITests(TestCase):
+    def test_api_home_status_code(self):
+        response = self.client.get("/api/")
+        self.assertEqual(response.status_code, 200)
+
